5be28c7061dbae2c2f8cd4968afa7b3dfd566fad
sheapa <55968932+sheapa@users.noreply.github.com>
2020-06-07 22:44:17 -0700

    MVP met. Seperated read/write file from api. Cleaned up api routes and reated store.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+const path = require("path");
+
+const router =require("express").Router();
+
+
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports=(router);
